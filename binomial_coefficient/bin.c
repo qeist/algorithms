0c43d5a71ff9700dc2c5ec9dc1ddb779a5f70050
qeist_ubuntu <qeist@hanmail.net>
2015-10-13 13:07:12 +0900

    binomial_coefficient

---
+++ binomial_coefficient/bin.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int bin(int n, int k);
+int minimum(int a, int b);
+int main(void)
+{
+	int n, k;	
+	printf("input n : ");
+	scanf("%d", &n);
+	printf("input k : ");
+	scanf("%d", &k);
+
+	printf("%dC%d is ... %d\n", n, k, bin(n, k));
+
+
+	
+
+
+	return 0;
+}
+
+int bin(int n, int k)
+{
+	int i, j;
+	int **arr;
+
+	arr = (int **)malloc(sizeof(int *) * n+1);
+	for (i = 0; i <= n; i++) {
+		arr[i] = (int *)malloc(sizeof(int) * k);
+	}
+
+	for (i = 0; i <= n; i++) {
+		for (j = 0; j <= minimum(i, k); j++) {
+			if (j == 0 || j == i)
+				arr[i][j] = 1;
+			else 
+				arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+		}
+	}
+	return arr[n][k];
+}
+
+int minimum(int a, int b)
+{
+	if (a > b)
+		return b;
+	else 
+		return a;
+}
